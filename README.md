d4b4dad6d14f2bc31f95c26168a720c07616d8f1
liujiaqi222 <z473487465@qq.com>
2021-09-11 23:51:22 +0800

    update

--- README.md
+++ README.md
@@ -1,7 +1,4 @@
-# Portfolio Responsive Complete
-## [Watch it on youtube](https://youtu.be/AKNvTxWOdKw)
-### Portfolio responsive complete
-A clean and nice web portfolio for designer or developer. That includes almost everything you want to show. At first, your name, links to social networks and a great image. Then the About section displays more information about you. Then the list of Skills it offers. It also has a section where it shows some recent work. And at the bottom it has a form, and at the end with a footer.
+# 个人简历网站
 
-Don't forget to join the channel for more videos like this.
-[Bedimcode](https://www.youtube.com/c/Bedimcode)
+在线访问：https://resume.jiaqicoder.com
+
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,15 @@
-/*===== MENU SHOW =====*/ 
-const showMenu = (toggleId, navId) =>{
+/*===== MENU SHOW =====*/
+const showMenu = (toggleId, navId) => {
     const toggle = document.getElementById(toggleId),
-    nav = document.getElementById(navId)
+        nav = document.getElementById(navId)
 
-    if(toggle && nav){
-        toggle.addEventListener('click', ()=>{
+    if (toggle && nav) {
+        toggle.addEventListener('click', () => {
             nav.classList.toggle('show')
         })
     }
 }
-showMenu('nav-toggle','nav-menu')
+showMenu('nav-toggle', 'nav-menu')
 
 
 
@@ -18,9 +18,9 @@ showMenu('nav-toggle','nav-menu')
 
 const skillsHeaders = document.querySelectorAll('.skills_header');
 
-skillsHeaders.forEach(el => el.addEventListener('click',()=>{
+skillsHeaders.forEach(el => el.addEventListener('click', () => {
     el.parentElement.classList.toggle('skills_close');
-    
+
 }))
 
 
@@ -33,16 +33,16 @@ skillsHeaders.forEach(el => el.addEventListener('click',()=>{
 
 
 /*===== ACTIVE AND REMOVE MENU =====*/
-const navLink = document.querySelectorAll('.nav__link');   
+const navLink = document.querySelectorAll('.nav__link');
 
-function linkAction(){
-  /*Active link*/
-  navLink.forEach(n => n.classList.remove('active'));
-  this.classList.add('active');
-  
-  /*Remove menu mobile*/
-  const navMenu = document.getElementById('nav-menu')
-  navMenu.classList.remove('show')
+function linkAction() {
+    /*Active link*/
+    navLink.forEach(n => n.classList.remove('active'));
+    this.classList.add('active');
+
+    /*Remove menu mobile*/
+    const navMenu = document.getElementById('nav-menu')
+    navMenu.classList.remove('show')
 }
 navLink.forEach(n => n.addEventListener('click', linkAction));
 
@@ -55,27 +55,27 @@ const sr = ScrollReveal({
 });
 
 /*SCROLL HOME*/
-sr.reveal('.home__title',{}); 
-sr.reveal('.button',{delay: 200}); 
-sr.reveal('.home__img',{delay: 400}); 
-sr.reveal('.home__social-icon',{ interval: 200}); 
+sr.reveal('.home__title', {});
+sr.reveal('.button', { delay: 200 });
+sr.reveal('.home__img', { delay: 400 });
+sr.reveal('.home__social-icon', { interval: 200 });
 
 /*SCROLL ABOUT*/
-sr.reveal('.about__img',{}); 
-sr.reveal('.about__subtitle',{delay: 400}); 
-sr.reveal('.about__text',{delay: 400}); 
+sr.reveal('.about__img', {});
+sr.reveal('.about__subtitle', { delay: 400 });
+sr.reveal('.about__text', { delay: 400 });
 
 /*SCROLL SKILLS*/
-sr.reveal('.skills__subtitle',{}); 
-sr.reveal('.skills__text',{}); 
-sr.reveal('.skills_info',{interval: 200}); 
-sr.reveal('.skills__img',{delay: 600});
+sr.reveal('.skills__subtitle', {});
+sr.reveal('.skills__text', {});
+sr.reveal('.skills_info', { interval: 200 });
+sr.reveal('.skills__img', { delay: 600 });
 
 /*SCROLL WORK*/
-sr.reveal('.work__img',{interval: 200}); 
+sr.reveal('.work__img', { interval: 200 });
 
 /*SCROLL CONTACT*/
-sr.reveal('.contact__input',{interval: 200}); 
+sr.reveal('.contact__input', { interval: 200 });
 
 
 
@@ -83,13 +83,86 @@ sr.reveal('.contact__input',{interval: 200});
 const mask = document.querySelector('.mask');
 const cancel = document.querySelector('.cancel')
 const wechat = document.querySelectorAll('.wechat');
-wechat.forEach(el =>{
-    el.addEventListener('click',(e)=>{
+wechat.forEach(el => {
+    el.addEventListener('click', (e) => {
         e.preventDefault();
         mask.classList.remove('isShow');
     })
 });
 
-cancel.addEventListener('click',()=>{
+cancel.addEventListener('click', () => {
     mask.classList.add('isShow')
+})
+
+// 轮播图
+const btn_l = document.querySelector('.button-left');
+const btn_r = document.querySelector('.button-right');
+const ul = document.querySelector('.carousel_list');
+// 设置图片序号 默认为0
+let id = 0;
+let isLock = false;
+
+const rightClick = () => {
+    if (isLock) return;
+    isLock = true;
+    id++;
+    // 加上动画
+    ul.style.transition = '0.5s';
+    ul.style.transform = `translateX(${-id * 25}%)`;
+    // 如果为第4张图片
+    if (id === 3) {
+        // 等待动画结束
+        id = 0;
+        setTimeout(() => { // 移动回第一张照片
+            ul.style.transform = `translateX(${-id * 25}%)`;
+            ul.style.transition = 'none'; // 悄悄的移动不要动画
+        }, 500);
+    }
+    setCircles(id);
+    setTimeout(() => {
+        isLock = false;
+    }, 500);
+}
+btn_r.addEventListener('click', rightClick);
+btn_l.addEventListener('click', () => {
+    if (isLock) return;
+    if (id === 0) {
+        ul.style.transform = 'translateX(-80%)';
+        ul.style.transition = 'none';
+        id = 3;
+    }
+    setTimeout(() => {
+        id--;
+        setCircles(id);
+        ul.style.transition = '0.5s';
+        ul.style.transform = `translateX(${-id * 25}%)`;
+    }, 0);
+    setTimeout(() => {
+        isLock = false;
+    }, 500);
+});
+
+const ol = document.querySelector('ol');
+function setCircles(index) {
+    [...ol.children].forEach(el => {
+        el.classList.remove('active');
+    })
+    ol.children[index].classList.add('active');
+}
+ol.addEventListener('click', e => {
+    // 如果点击的是li
+    if (e.target.tagName === 'LI') {
+        id = Number(e.target.getAttribute('data-n'));
+        ul.style.transform = `translate(${-id * 25}%)`;
+        setCircles(id);
+    }
+});
+// 定时器 自动轮播 模拟鼠标右点击
+let timer = setInterval(rightClick, 2000);
+ul.addEventListener('mouseover', () => {
+    clearInterval(timer);
+})
+ul.addEventListener('mouseleave', () => {
+    clearInterval(timer);
+    timer = setInterval(rightClick, 2000);
 })
